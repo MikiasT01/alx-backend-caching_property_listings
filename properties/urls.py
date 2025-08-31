d45d99ef586eaf9ad7a5fa8f37e7abecc798ee4c
mikias t <mteklu01@gmail.com>
2025-08-31 19:46:03 +0300

    update

--- properties/urls.py
+++ properties/urls.py
@@ -1,6 +1,9 @@
-from django.urls import path
-from .views import property_list
+from django.core.cache import cache
+from .models import Property
 
-urlpatterns = [
-        path('', property_list, name='property_list'),
-    ]
+def get_all_properties():
+        properties = cache.get('all_properties')
+        if properties is None:
+            properties = Property.objects.all()
+            cache.set('all_properties', properties, 3600)  # Cache for 1 hour
+        return properties
