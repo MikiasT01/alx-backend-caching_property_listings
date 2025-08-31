7403619fa301c12b9750d96e8b650a2b7964c0cf
mikias t <mteklu01@gmail.com>
2025-08-31 20:02:24 +0300

    update

--- properties/utils.py
+++ properties/utils.py
@@ -1,9 +1,9 @@
 from django.core.cache import cache
 from .models import Property
 
-def getallproperties():
-        properties = cache.get('allproperties')
+def get_all_properties():
+        properties = cache.get('all_properties')
         if properties is None:
             properties = Property.objects.all()
-            cache.set('allproperties', properties, 3600)  # Cache for 1 hour
+            cache.set('all_properties', properties, 3600)  # Cache for 1 hour
         return properties
